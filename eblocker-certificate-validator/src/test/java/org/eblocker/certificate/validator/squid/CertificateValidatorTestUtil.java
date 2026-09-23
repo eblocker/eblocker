a553643ca88f0290837c1f6d4072d297a590cbd9
eBlocker Open Source <dev@eblocker.org>
2026-09-23 10:34:55 +0200

    Update expired MS test certificate (with new chain)

--- eblocker-certificate-validator/src/test/java/org/eblocker/certificate/validator/squid/CertificateValidatorTestUtil.java
+++ eblocker-certificate-validator/src/test/java/org/eblocker/certificate/validator/squid/CertificateValidatorTestUtil.java
@@ -30,7 +30,7 @@ public class CertificateValidatorTestUtil {
     private static AtomicLong nextRequestId = new AtomicLong();
 
     public static CertificateValidationRequest createValidRequest(boolean useConcurrency) throws IOException, CryptoException {
-        return createRequest(useConcurrency, "www.microsoft.com", "www.microsoft.com.cert", "MicrosoftAzureTLSIssuingCA04.cert");
+        return createRequest(useConcurrency, "www.microsoft.com", "www.microsoft.com.cert", "Microsoft_TLS_G2_RSA_CA_OCSP_04.cert", "Microsoft_TLS_RSA_Root_G2.cert");
     }
 
     public static CertificateValidationRequest createInvalidRequest(boolean useConcurrency) throws IOException, CryptoException {
